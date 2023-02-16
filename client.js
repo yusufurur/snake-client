d2f6b5315837cbd404967fa628a9f9e074433e5f
yusufurur <yusufurur@gmail.com>
2023-02-15 22:00:54 -0500

    Snake Initials

--- client.js
+++ client.js
@@ -1,4 +1,5 @@
 const net = require("net");
+const readline = require("readline");
 const connect = function () {
   const conn = net.createConnection({
     host: "165.227.47.243",
@@ -7,9 +8,10 @@ const connect = function () {
 
   // interpret incoming data as text
   conn.setEncoding("utf8");
-
+  
   conn.on("connect", () => {
-    console.log("connected to the Server")
+    console.log("Successfully connected to game server");
+    conn.write("Name: ABC");
   });
   return conn;
 };
